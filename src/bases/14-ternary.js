51908a2f5a042a24d7586fb1d200be4156371262
zack <hackelian89@gmail.com>
2022-06-10 22:34:51 -0600

    ternary & async-b agregados

---
+++ src/bases/14-ternary.js
@@ -0,0 +1,22 @@
+
+
+let firstName = 'Juan';
+let lastName;
+
+// console.log(`${ firstName || 'No firstName'} ${ lastName || 'No lastName' }`);
+
+const isActive = true
+
+// if ( isActive ) {
+//     msg = 'Esta activo 😀'
+// } else {
+//     'Esta inactivo 😥'
+// }
+
+const msg = ( isActive )
+                ? 'Esta activo 😀' 
+                : 'Esta inactivo 😥'
+
+
+console.log(msg);
+
