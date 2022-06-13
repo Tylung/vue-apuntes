2a99afaabf4d15c3f2f4759931d238aac8fe830d
zack <hackelian89@gmail.com>
2022-06-13 16:03:27 -0500

    index & app.js actualizados

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
+const quotes = [
+    { quote: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'I believe what doesn’t kill you simply makes you, stranger.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', author: 'Henri Ducard, Batman Begins' },
+    { quote: 'You either die a hero or live long enough to see yourself become the villain.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'If you’re good at something, never do it for free.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Yes, father. I shall become a bat.', author: 'Bruce Wayne/Batman, Batman: Year One' },
+]
+
+console.log({quotes});
 
 const app = Vue.createApp({
-    // template: `
-        // <h1> Holas </h1>
-    // `
-    // methods: {},
-    // watch: {},
-    // options api
     data() {
         return {
-            quote: "I'm Batman",
-            author: "Bruce Wayne"
+            quotes,
         }
     },
-    methods: {
-        changeQuote() {
-            console.log('click');
-            this.author = 'Juan el Taquero'
-            this.capitalize()
-        },
-        capitalize(){
-            this.quote = this.quote.toUpperCase()
-        }
-    }
-
 })
+
 
 app.mount('#myApp')
