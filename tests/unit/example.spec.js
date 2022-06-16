3922f0aa090fc81413e15c65814318eb9b3441c4
zack <hackelian89@gmail.com>
2022-06-16 10:18:11 -0500

    primera prueba

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,24 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+
+describe('Example Component', () => {
+
+  // hacer una evaluacion pequeña
+  test( 'Debe de ser mayor a 10', () => {
+
+    // Arreglar - Arrange
+    let value = 5;
+
+    // Estimulo
+    value = value + 2
+
+    // Assert - Observar el resultado
+
+    if ( value > 10) {
+      // TODO: Todo bien
+    } else {
+      throw `${ value } no es mayor a 10`
+    }
   })
+
 })
+
