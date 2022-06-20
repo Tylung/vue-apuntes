5f0242e83ad71af2ba5bbc8818c85ffaf58e2b12
zack <hackelian89@gmail.com>
2022-06-20 13:10:26 -0500

    test refactorizado

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -4,9 +4,14 @@ import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
 
-    test('h2 debe tener el valor por defecto "Counter"', () => {
+    let wrapper = 
 
-        const wrapper = shallowMount( Counter )
+
+    beforeEach( () => {
+        wrapper = shallowMount( Counter )
+    } )
+
+    test('h2 debe tener el valor por defecto "Counter"', () => {
 
         expect( wrapper.find("h2").exists() ).toBeTruthy()
 
@@ -16,32 +21,27 @@ describe('Counter Component', () => {
     })
 
 
-    test('el valor por defecto debe de ser 100 en el p', () => {
-        const wrapper = shallowMount( Counter )
+    test('el valor por defecto debe de ser 100 en el p', async () => {
+
         const value = wrapper.find( '[data-test-id="counter"]' ).text()
         expect( value ).toBe('100')
+
     })
 
 
     test('debe de incrementar y decrementar el valor del contador', async () => {
-        const wrapper = shallowMount( Counter )
         
-        const increaseBtn = wrapper.find('button')
+        const [increaseBtn, decreaseBtn]  = wrapper.findAll('button') 
 
         await increaseBtn.trigger('click')
         await increaseBtn.trigger('click')
         await increaseBtn.trigger('click')
-
-        let value = wrapper.find( '[data-test-id="counter"]' ).text()
-
-        // TODO: Tarea
-        const decreaseBtn = wrapper.findAll('button')[1]
 
         await decreaseBtn.trigger('click')
         await decreaseBtn.trigger('click')
-
         
-        value = wrapper.find( '[data-test-id="counter"]' ).text()
+        const value = wrapper.find( '[data-test-id="counter"]' ).text()
+
         expect( value ).toBe('101')
     })
 })
