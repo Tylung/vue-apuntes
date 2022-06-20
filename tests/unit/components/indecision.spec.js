44c300199522181c73e2ad722e601d1963be828d
zack <hackelian89@gmail.com>
2022-06-20 14:43:21 -0500

    global fetch & valid api

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -8,10 +8,20 @@ describe('Indecision Component', () => {
     let wrapper
     let clgSpy
 
+    global.fetch = jest.fn( () => Promise.resolve({
+        json: () => Promise.resolve({
+            answer: 'yes',
+            forced: false,
+            image: 'https://yesno.wtf/assets/yes/12-e4f57c8f172c51fdd983c2837349f853.gif'
+        })
+    }))  
+
     beforeEach( () => {
         wrapper = shallowMount( Indecision )
 
         clgSpy = jest.spyOn( console, 'log' )
+
+        jest.clearAllMocks()
 
     }) 
 
@@ -30,12 +40,26 @@ describe('Indecision Component', () => {
 
     })
 
-    test("escribir el simbolo '?' debe disparar el fetch", () => {
-        
+    test("escribir el simbolo '?' debe disparar el fetch", async () => {
+        const getAnswerSpy = jest.spyOn( wrapper.vm, 'getAnswer' )
+        const input = wrapper.find('input')
+        await input.setValue('Hola Mundo?')
+
+        expect( clgSpy ).toHaveBeenCalledTimes(1)
+
+        expect( getAnswerSpy ).toHaveBeenCalled()
     })
 
-    test('pruebas en getAnswer', () => {
-        
+    test('pruebas en getAnswer', async () => {
+        await wrapper.vm.getAnswer()
+
+        // console.log( wrapper.vm.img );
+        // console.log( wrapper.vm.answer );
+        const img = wrapper.find('img')
+
+        expect( img.exists() ).toBeTruthy()
+        expect( wrapper.vm.img ).toBe('https://yesno.wtf/assets/yes/12-e4f57c8f172c51fdd983c2837349f853.gif')
+        expect( wrapper.vm.answer ).toBe('Si!')
     })
 
     test('pruebas en getAnswer - fallo en el API', () => {
