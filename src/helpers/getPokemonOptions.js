021e2c2d75db1b7889489ddbd084b71a167914a9
zack <hackelian89@gmail.com>
2022-06-21 11:57:02 -0500

    get pokemons axios

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,4 +1,4 @@
-
+import pokemonApi from '@/api/pokemonApi';
 
 const getPokemons = () => {
 
@@ -12,21 +12,35 @@ const getPokemons = () => {
 
 
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async () => {
 
     const mixedPokemons = getPokemons().sort( () => Math.random() - 0.5 )
 
-    getPokemonNames( mixedPokemons.splice(0, 4) )
+    const pokemons = await getPokemonNames( mixedPokemons.splice(0, 4) )
 
-
-
-
+    return pokemons
 }
 
-const getPokemonNames = ( [a, b, c, d  ] = [] ) => {
+const getPokemonNames = async ( [a, b, c, d  ] = [] ) => {
 
-    console.log({a, b, c, d});
-    
+    // const { data } = await pokemonApi.get(`/3`)
+    // console.log( data.name, data.id);
+    // console.log({a, b, c, d});
+    const arrPromises = [
+        pokemonApi.get(`/${ a }`),
+        pokemonApi.get(`/${ b }`),
+        pokemonApi.get(`/${ c }`),
+        pokemonApi.get(`/${ d }`)
+    ]
+
+    const [p1, p2, p3, p4 ] = await Promise.all( arrPromises )
+
+    return [
+        { name: p1.data.name, id: p1.data.id},
+        { name: p2.data.name, id: p2.data.id},
+        { name: p3.data.name, id: p3.data.id},
+        { name: p4.data.name, id: p4.data.id}
+    ]
 }
 
 
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -15,7 +15,7 @@ import PokemonOptions from '@/components/PokemonOptions'
 
 import getPokemonOptions from '@/helpers/getPokemonOptions'
 
-console.log( getPokemonOptions() )
+// console.log( getPokemonOptions() )
 
 export default {
     components: { PokemonPicture, PokemonOptions }
