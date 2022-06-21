a302793d23ee05c96cca390be061183b0c7a0160
zack <hackelian89@gmail.com>
2022-06-21 09:35:25 -0500

    estructura pokemon game

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img class="fade-in" alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PokemonPage />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonPage from './pages/PokemonPage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    PokemonPage,
+}
 }
 </script>
 
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,27 @@
+
+<template>
+
+    <h1>¿Quien es este pokémon?</h1>
+
+    <PokemonImg />
+
+    <PokemonOptions />
+
+</template>
+
+<script>
+import PokemonImg  from '@/components/PokemonImg'
+import PokemonOptions from '@/components/PokemonOptions'
+
+
+
+export default {
+    components: { PokemonImg, PokemonOptions }
+}
+</script>
+
+
+
+
+
+
