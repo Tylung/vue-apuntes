68b11948356c6ed686a668c11570924640dd701b
zack <hackelian89@gmail.com>
2022-06-21 15:22:57 -0500

    test axios pokemon endpoint

---
+++ tests/unit/api/pokemonApi.spec.js
@@ -0,0 +1,16 @@
+import pokemonApi from '@/api/pokemonApi';
+
+describe('pokemonApi', () => {
+
+    test('axios debe estar configurado con el api de pokemon', () => {
+        
+        expect( pokemonApi.defaults.baseURL )
+            .toBe( 'https://pokeapi.co/api/v2/pokemon' )
+
+    })
+
+})
+
+
+
+
