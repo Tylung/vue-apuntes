5a4f9db2a981e04b66c9e306507e25707df79a9c
zack <hackelian89@gmail.com>
2022-06-22 14:03:17 -0500

    mock pokemons & test pokemonOptions & emitted

---
+++ tests/unit/mocks/pokemons.mock.js
@@ -0,0 +1,18 @@
+export const pokemons = [
+    { 
+        name: 'bulbasaur', 
+        id: 1 
+    },
+    { 
+        name: 'charmander', 
+        id: 4 
+    },
+    { 
+        name: 'mew', 
+        id: 151 
+    },
+    { 
+        name: 'mewtwo', 
+        id: 150 
+    }
+]
