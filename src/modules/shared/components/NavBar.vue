44057a11a6191d3c1c54ef29680824d5757601e3
zack <hackelian89@gmail.com>
2022-06-27 12:24:03 -0500

    NavBar component & router-links agregados

---
+++ src/modules/shared/components/NavBar.vue
@@ -0,0 +1,31 @@
+
+<template>
+
+    <div>
+        <router-link to="/">PokemonList</router-link>
+        <router-link to="/id">Pokemon by Id</router-link>
+        <router-link to="/about">About</router-link>
+    </div>
+
+</template>
+
+
+
+<style scoped>
+
+div {
+    list-style: none;
+    padding: 0 30px;
+}
+
+div a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: 0 10px;
+}
+
+a.router-link-exact-active {
+    color: #42b983;
+}
+
+</style>
