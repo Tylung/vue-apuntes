bd0721d2b19aea672ca77c1886245a30b08e6371
zack <hackelian89@gmail.com>
2022-06-27 11:13:39 -0500

    404 page con router

--- src/router/router.js
+++ src/router/router.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import AboutPage from '@/modules/pokemon/pages/AboutPage';
 import ListPage from '@/modules/pokemon/pages/ListPage';
 import PokemonPage from '@/modules/pokemon/pages/PokemonPage';
+import NoPageFound from '@/modules/shared/pages/NoPageFound';
 
 const routes = [
     { path: '/', component: ListPage },
     { path: '/about', component: AboutPage },
     { path: '/id', component: PokemonPage },
+    { path: '/:pathMatch(.*)*', component: NoPageFound },
   ]
 
 const router = createRouter({
