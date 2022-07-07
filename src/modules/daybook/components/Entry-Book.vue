8c810f117fc8a821401118e023ade3489a429898
zack <hackelian89@gmail.com>
2022-07-07 13:15:41 -0500

    EntryList & EntryBook

---
+++ src/modules/daybook/components/Entry-Book.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="entry-container mb-3 pointer p-2"
+  >
+  <!-- titulo -->
+    <div class="entry-title d-flex">
+        <span class="text-success fs-5 fw-bold"> 7 </span>
+        <span class="mx-1 fs-5"> Julio </span>
+        <span class="mx-2 fw-light"> 2022, Jueves </span>
+    </div>
+
+    <div class="entry-description">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo, accusantium eum eius est dolores aspernatur error reprehenderit rerum doloribus quam eligendi blanditiis quos iste. Saepe illum ipsum itaque neque mollitia!
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: #808080, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+
+</style>
--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>DayBook Layout</h1>
+    <Navbar />
+
+    <div class="d-flex">
+        <div class="col-4">
+            <EntryLIst/>
+        </div>
+        <div class="col">
+            <router-view />
+        </div>
+    </div>
 </template>
+
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+    components: {
+    Navbar: defineAsyncComponent(() => import("../components/Nav-bar.vue")),
+    EntryLIst: defineAsyncComponent(() => import('../components/Entry-LIst.vue'))
+}
+}
+
+</script>
+
