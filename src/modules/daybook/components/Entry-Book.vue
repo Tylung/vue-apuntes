ab6d3ee4d4ea98b2a9851c10aa9d7b2c2672cc68
zack <jonaenglish64@gmail.com>
2022-07-18 13:31:42 -0600

    Entry props computed

--- src/modules/daybook/components/Entry-Book.vue
+++ src/modules/daybook/components/Entry-Book.vue
@@ -1,42 +1,65 @@
 <template>
-  <div
-    class="entry-container mb-3 pointer p-2"
-    @click="$router.push({ name: 'entry',  params: { id: 10 } })"
-  >
-  <!-- titulo -->
-    <div class="entry-title d-flex">
-        <span class="text-success fs-5 fw-bold"> 7 </span>
-        <span class="mx-1 fs-5"> Julio </span>
-        <span class="mx-2 fw-light"> 2022, Jueves </span>
-    </div>
+	<div class="entry-container pointer p-2" @click="$router.push({ name: 'entry', params: { id: entry.id } })">
+		<!-- titulo -->
+		<div class="entry-title d-flex">
+			<span class="text-success fs-5 fw-bold"> {{day}} </span>
+			<span class="mx-1 fs-5"> {{ month }} </span>
+			<span class="mx-2 fw-light"> {{ yearDate }} </span>
+		</div>
 
-    <div class="entry-description">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo, accusantium eum eius est dolores aspernatur error reprehenderit rerum doloribus quam eligendi blanditiis quos iste. Saepe illum ipsum itaque neque mollitia!
-    </div>
+		<div class="entry-description">
+			{{ shortText }}
+		</div>
 
-  </div>
+	</div>
 </template>
 
 <script>
-export default {
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+const days   = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
 
+export default {
+	props: {
+		entry: {
+			type: Object,
+			required: true
+		}
+	},
+
+	computed: {
+		shortText() {
+			return ( this.entry.text.length > 120 )
+				? this.entry.text.substring(0,120) + '...'
+				: this.entry.text 
+		},
+		day() {
+			const date = new Date( this.entry.date )
+			return date.getDate()
+		},
+		month() {
+			const date = new Date( this.entry.date )
+			return months[ date.getMonth() ]
+		},
+		yearDate() {
+			const date = new Date( this.entry.date )
+			return `${ date.getFullYear() }, ${ days[ date.getDay() ] }`
+		}
+	}
 }
 </script>
 
 <style lang="scss" scoped>
-
 .entry-container {
-    border-bottom: 1px solid #2c3e50;
-    transition: 0.2s all ease-in;
+	border-bottom: 1px solid #2c3e50;
+	transition: 0.2s all ease-in;
 
-    &:hover {
-        background-color: lighten($color: #808080, $amount: 45);
-        transition: 0.2s all ease-in;
-    }
+	&:hover {
+		background-color: lighten($color: #808080, $amount: 45);
+		transition: 0.2s all ease-in;
+	}
 
-    .entry-description {
-        font-size: 12px;
-    }
+	.entry-description {
+		font-size: 12px;
+	}
 }
-
 </style>
