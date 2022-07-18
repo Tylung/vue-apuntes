273e3daff0b0a00c0aed20581a675dc2f0d96234
zack <jonaenglish64@gmail.com>
2022-07-18 17:00:49 -0600

    Mostrar Entry & redireccionar usuario

--- src/modules/daybook/views/Entry-View.vue
+++ src/modules/daybook/views/Entry-View.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="entry-title d-flex justify-content-between p-2">
+
     <div>
-        <span class="text-success fs-3 fw-bold">15</span>
-        <span class="mx-1 fs-3">Julio</span>
-        <span class="mx-2 fs-4 fw-light">2022, Jueves</span>
+        <span class="text-success fs-3 fw-bold"> {{ day }} </span>
+        <span class="mx-1 fs-3"> {{ month }} </span>
+        <span class="mx-2 fs-4 fw-light"> {{ yearDay }} </span>
     </div>
 
     <div>
@@ -22,7 +23,9 @@
 
     <hr>
     <div class="d-flex flex-column px-3 h-75">
-        <textarea placeholder="¿Qué sudedió hoy?"></textarea>
+        <textarea placeholder="¿Qué sudedió hoy?"
+            v-model="entry.text">
+        </textarea>
     </div>
 
     <Fab 
@@ -40,6 +43,7 @@
 <script>
 import { defineAsyncComponent } from 'vue';
 import { mapGetters } from 'vuex';
+import getDayMonthYear from '../helpers/getDayMonthYear'
 
 export default {
     props: {
@@ -52,15 +56,35 @@ export default {
     components: {
         Fab: defineAsyncComponent(() => import('../components/Float-Btn.vue') )
     },
+    
+    data() {
+        return {
+            entry: null
+        }
+    },
 
     computed: {
         ...mapGetters('journal', ['getEntryById']),
+        day() {
+            const { day } = getDayMonthYear( this.entry.date )
+            return day
+        },
+        month() {
+            const { month } = getDayMonthYear( this.entry.date )
+            return month
+        },
+        yearDay() {
+            const { yearDay } = getDayMonthYear( this.entry.date )
+            return yearDay
+        }
     },
 
     methods: {
         loadEntry(  ) {
             const entry = this.getEntryById( this.id )
-            console.log( entry )
+            if ( !entry ) this.$router.push({ name: 'no-entry' })
+
+            this.entry = entry
         }
     },
 
