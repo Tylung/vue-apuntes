ea76bb2c1578cec9de74e7a3d062f47d9034d5db
zack <jonaenglish64@gmail.com>
2022-07-18 19:57:55 -0600

    Mostrar loading

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,7 +1,18 @@
 <template>
     <Navbar />
 
-    <div class="d-flex">
+    <div 
+        class="row justify-content-md-center" v-if="isLoading">
+        <div class="col-3 alert-info text-center mt-5">
+            Espere por favor...
+        <h3 class="mt-2">
+            <i class="fa fa-spin fa-sync"></i>
+        </h3>
+        </div>
+    </div>
+
+    <div 
+        class="d-flex" v-else>
         <div class="col-4">
             <EntryList/>
         </div>
@@ -14,12 +25,16 @@
 
 <script>
 import { defineAsyncComponent } from 'vue'
-import { mapActions } from 'vuex';
+import { mapActions, mapState } from 'vuex';
 
 export default {
     components: {
     Navbar: defineAsyncComponent(() => import("../components/Nav-bar.vue")),
     EntryList: defineAsyncComponent(() => import('../components/Entry-List.vue'))
+    },
+
+    computed: {
+        ...mapState('journal', ['isLoading'])
     },
 
     methods: {
