b83674b666354f758033a1f035c1a3b9d9bdacb1
Jonathan <jonaenglish64@gmail.com>
2022-07-20 12:18:12 -0500

    Borrar imagen subida por pruebas

--- tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -1,8 +1,17 @@
+import cloudinary from 'cloudinary'
+import axios from 'axios'
+
 import uploadImage from '@/modules/daybook/helpers/uploadImage'
-import axios from 'axios';
+
+cloudinary.config({
+    cloud_name: 'drelgczps',
+    api_key: '813892189533876',
+    api_secret: 'UQRchbESxnC8fxf5vV2LQQiwSME'
+})
+
 
 describe('Pruebas en el upload image', () => {
-    test('debe retornar un archivo y retornar el url', async () => {
+    test('debe retornar un archivo y retornar el url', async (  ) => {
 
         const {data} = await axios.get('https://res.cloudinary.com/drelgczps/image/upload/v1658252457/cld-sample-2.jpg', {
             responseType: 'arraybuffer'
@@ -13,6 +22,17 @@ describe('Pruebas en el upload image', () => {
         const url = await uploadImage( file )
 
         expect( typeof( url ) ).toBe('string')
+
+
+        // tomar el id
+
+        const segments = url.split('/')
+        const imgId = segments[ segments.length -1 ].replace('.jpg', '')
+
+        cloudinary.v2.api.delete_resources( imgId, {}, () => {
+            // done()
+            // no se puede usar done junto a sync/await a partir de la version 26.6
+        })
 
     })
 })
