a25abecb288110548afb848c35c2c90d7eb63e89
Jonathan <jonaenglish64@gmail.com>
2022-07-20 13:12:39 -0500

    Test Mutation UpdateEntry

--- tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -44,5 +44,32 @@ describe('Vuex - Pruebas en el Journal Module', () => {
         
     })
 
+    test('Mutation: Update Entry', () => {
+        //  crear store
+        const store = createVuexStore( journalState  )
+
+        const journal = store.state.journal
+
+        // updatedEntry
+        const updatedEntry = {
+            date: 1658248711828,
+            "picture": "https://res.cloudinary.com/drelgczps/image/upload/v1658255408/ygwxuwn6nj1pwy76plju.jpg",
+            id: "-N7MJs8HNasey4zAB6zm",
+            text: "Hola Dev! desde mock data"
+        }
+
+        // commit de la mutation
+        store.commit( 'journal/updateEntry', updatedEntry )
+
+        // Expects
+        // Entries.lenght = 2
+        expect( journal.entries.length ).toBe( 2 )
+        // Entries tiene que existir en UpdatedEntry toEqual
+        const entry = journal.entries.find( e => e.id === updatedEntry.id )
+        
+        expect( updatedEntry ).toEqual( entry )
+
+    })
+
     
 })
